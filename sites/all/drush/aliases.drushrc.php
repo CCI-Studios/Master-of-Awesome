6140461fb8096357324aa4710cc4a4b919370a29
Chris Morris <cmorris@ccistudios.com>
2014-02-28 15:55:16 -0500

    Initial Commit

---
+++ sites/all/drush/aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+
+$aliases['dev'] = array(
+	'uri'=> 'dev.masterofawesome.ca',
+	'root' => '/home/moa/subdomains/dev/public_html',
+	'remote-host'=> 'host.lambtonshield.com',
+	'remote-user'=> 'moa',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	)
+);
---
+++ sites/all/themes/moa/js/slideshow.js
@@ -0,0 +1,85 @@
+var slideshowTimer;
+var slideshowCurrent = 1;
+
+(function($) {
+	$(function(){
+		$("#block-views-home-header-images-block a.next").click(clickNext);
+		$("#block-views-home-header-images-block a.previous").click(clickPrevious);
+
+		if ($("body").hasClass("front"))
+			slideshowTimer = setInterval(nextInterval, 6000);
+	});
+
+	function clickNext()
+	{
+		stop();
+		next($($("#block-views-home-header-images-block .views-row").get(slideshowCurrent-1)));
+		return false;
+	}
+
+	function clickPrevious()
+	{
+		stop();
+		previous($($("#block-views-home-header-images-block .views-row").get(slideshowCurrent-1)));
+		return false;
+	}
+
+	function previous($row)
+	{
+		console.log("pevious()");
+		console.log($row);
+		var $previous = $row.prev();
+		if ($previous.length == 0)
+			$previous = $row.parent().children().last();
+		var rowWidth = $row.width();
+		slideshowCurrent = $previous.index()+1;
+
+		$row.css({
+			"left":"0"
+		}).animate({
+			"left":rowWidth+"px"
+		});
+
+		$previous.css({
+			"left":"-"+rowWidth+"px",
+			"display":"block"
+		}).animate({
+			"left":"0"
+		});
+	}
+
+	function next($row)
+	{
+		console.log("next()");
+		console.log($row);
+		var $next = $row.next();
+		if ($next.length == 0)
+			$next = $row.parent().children().first();
+		var rowWidth = $row.width();
+		slideshowCurrent = $next.index()+1;
+
+		$row.css({
+			"left":"0"
+		}).animate({
+			"left":"-"+rowWidth+"px"
+		});
+
+		$next.css({
+			"left":rowWidth+"px",
+			"display":"block"
+		}).animate({
+			"left":"0"
+		});
+	}
+
+	function nextInterval()
+	{
+		console.log("nextInterval()");
+		next($($("#block-views-home-header-images-block .views-row").get(slideshowCurrent-1)));
+	}
+
+	function stop()
+	{
+		clearInterval(slideshowTimer);
+	}
+}(jQuery));
