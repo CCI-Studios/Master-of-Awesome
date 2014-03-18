34a0b6e5e41a3590e43296af0d876c28015140e7
Chris Morris <cmorris@ccistudios.com>
2014-03-18 15:10:44 -0400

    Added webforms, widget headings are links

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -9,3 +9,13 @@ $aliases['dev'] = array(
 		'%files'=> 'sites/default/files',
 	)
 );
+
+$aliases['live'] = array(
+	'uri'=> 'live.masterofawesome.ca',
+	'root' => '/home/moa/subdomains/live/public_html',
+	'remote-host'=> 'host.lambtonshield.com',
+	'remote-user'=> 'moa',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	)
+);
